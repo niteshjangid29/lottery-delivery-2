1ec1ed7c55d3d83e2777d8263253b36cc6bbff46
Divyakumar6163 <divyakumar768800@gmail.com>
2024-12-29 05:01:37 +0530

    redeploy

--- server/app.js
+++ server/app.js
@@ -21,6 +21,7 @@ app.use(
       "https://lottery-delivery.vercel.app",
       "https://api.lottog.shop",
       "https://admin.lottog.shop",
+      "https://www.admin.lottog.shop",
       "https://lottery-delivery-2-backend.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
