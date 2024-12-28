be1583697f460229f1523f41068b651e6c6384d9
Divyakumar6163 <divyakumar768800@gmail.com>
2024-12-28 21:37:51 +0530

    Twilio fix

--- server/config/twilio.js
+++ server/config/twilio.js
@@ -4,9 +4,9 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 // Log the credentials for debugging (remove in production)
-console.log("TWILIO_SID:", process.env.TWILIO_SID);
-console.log("TWILIO_AUTH_TOKEN:", process.env.TWILIO_AUTH_TOKEN);
-console.log("TWILIO_PHONE_NUMBER:", process.env.TWILIO_PHONE_NUMBER);
+// console.log("TWILIO_SID:", process.env.TWILIO_SID);
+// console.log("TWILIO_AUTH_TOKEN:", process.env.TWILIO_AUTH_TOKEN);
+// console.log("TWILIO_PHONE_NUMBER:", process.env.TWILIO_PHONE_NUMBER);
 
 if (
   !process.env.TWILIO_SID ||
@@ -20,7 +20,7 @@ if (
 const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
 
 const sendOtpViaTwilio = async (phone, otp) => {
-  console.log(phone, otp);
+  // console.log(phone, otp);
   try {
     const message = await client.messages.create({
       body: `Your OTP code is ${otp}`,
@@ -28,7 +28,7 @@ const sendOtpViaTwilio = async (phone, otp) => {
       // from: "+1 402 625 6628",
       to: phone,
     });
-    console.log(message);
+    // console.log(message);
     return { success: true, message: message.sid };
   } catch (error) {
     console.error("Error sending OTP via Twilio:", error);
